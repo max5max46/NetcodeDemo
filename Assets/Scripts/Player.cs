5e20acdb072d40890c49825a8812e8971b5656fb
Dylan Adams <113365542+max5max46@users.noreply.github.com>
2025-02-04 09:38:27 -0400

    Added Second Player and Synced Object

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,105 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
+    [Header("Properties")]
+    [SerializeField] private float maxSpeed = 5;
+    [SerializeField] private float acceleration = 10;
+    [SerializeField] private float decelerationMultiplier = 0.8f;
+
+    private Rigidbody2D rb;
+    private Vector2 movementVector;
+
+    private bool upPressed;
+    private bool downPressed;
+    private bool rightPressed;
+    private bool leftPressed;
+
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            enabled = false;
+            return;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        ManageInputs(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ManageInputs();
+
+        SetMovementVector();
+
+        ManageInputs(true);
     }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.magnitude < maxSpeed)
+            rb.AddRelativeForce(movementVector);
+
+        // Increases deceleration to prevent sliding
+        rb.velocity = new Vector2(rb.velocity.x * decelerationMultiplier, rb.velocity.y * decelerationMultiplier);
+    }
+
+    private void ManageInputs(bool resetInputs = false)
+    {
+        if (!resetInputs)
+        {
+            if (Input.GetKey(KeyCode.W))
+                upPressed = true;
+
+            if (Input.GetKey(KeyCode.S))
+                downPressed = true;
+
+            if (Input.GetKey(KeyCode.D))
+                rightPressed = true;
+
+            if (Input.GetKey(KeyCode.A))
+                leftPressed = true;
+        }
+        else
+        {
+            upPressed = false;
+            downPressed = false;
+            rightPressed = false;
+            leftPressed = false;
+        }
+    }
+
+    private void SetMovementVector()
+    {
+        // Resets force adding variables to zero for the next add force
+        float moveForwardBackward = 0;
+        float moveLeftRight = 0;
+
+        // Turns WASD input into the add force variables 
+        if (upPressed)
+            moveForwardBackward += 1;
+
+        if (downPressed)
+            moveForwardBackward -= 1;
+
+        if (rightPressed)
+            moveLeftRight += 1;
+
+        if (leftPressed)
+            moveLeftRight -= 1;
+
+        movementVector = new Vector2(moveLeftRight, moveForwardBackward).normalized * acceleration;
+    }
+
 }
